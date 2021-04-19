a3e9fcfcacf74bd0b36b6f0a0b8c571b16e81deb
Hongbin-L <60090360+Hongbin-L@users.noreply.github.com>
2021-04-18 20:57:26 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const http = require("http");
 const data = require("./data");
-const querystring = require("querystring");
+const qs = require("querystring");
 const server = http.createServer((req, res) => {
-  var path = req.url.toLowerCase();
+  let path = req.url.toLowerCase().split("?");
   switch (path[0]) {
     case "/":
       res.writeHead(200, { "Content-Type": "text/plain" });
       res.end(JSON.stringify(data.getAll()));
       break;
       case "/detail":
-        let param = querystring.parse(path[1]);
-        res.end(JSON.stringify(data.getItem(param.brand)));
+        let query = qs.parse(path[1]);
+        res.end(JSON.stringify(data.getItem(query.brand)));
         break;
       case "/about":
       res.writeHead(200, { "Content-Type": "text/plain" });
